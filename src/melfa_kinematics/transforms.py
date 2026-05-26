7387c88658ec3e3edc8da22696f5521d8758cb8f
Daniel Florez <daniel.florezm@uniagustiniana.edu.co>
2026-05-26 17:51:19 -0500

    Add homogeneous rotation transformations

--- src/melfa_kinematics/transforms.py
+++ src/melfa_kinematics/transforms.py
@@ -1,13 +1,50 @@
 import numpy as np
 
 def Trans(x, y, z):
-    T = np.array([
-       [1, 0, 0, x],
-       [0, 1, 0, y],
-       [0, 0, 1, z],
-       [0, 0, 0, 1]
-    ])
+   T = np.array([
+   [1, 0, 0, x],
+   [0, 1, 0, y],
+   [0, 0, 1, z],
+   [0, 0, 0, 1]
+   ])
+
+   return T
 
 
-    return T
+
+
+def RotX(theta):
+   R = np.array([
+   [1, 0, 0, 0],
+   [0, np.cos(theta), -np.sin(theta), 0],
+   [0, np.sin(theta), np.cos(theta), 0],
+   [0, 0, 0, 1]
+   ])
+
+
+   return R
+
+
+def RotY(theta):
+   R = np.array([
+   [np.cos(theta), 0, np.sin(theta), 0],
+   [0, 1, 0, 0],
+   [-np.sin(theta), 0, np.cos(theta), 0],
+   [0, 0, 0, 1]
+   ])
+
+
+   return R
+
+
+def RotZ(theta):
+   R = np.array([
+   [np.cos(theta), -np.sin(theta), 0, 0],
+   [np.sin(theta), np.cos(theta), 0, 0],
+   [0, 0, 1, 0],
+   [0, 0, 0, 1]
+   ])
+
+
+   return R
 
